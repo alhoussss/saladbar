10c26b6ae70195b4f504eac62dc75f5dd372d000
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2022-12-06 23:36:17 +0100

    formulaire

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+          <div className="App">
+      <header>
+        <h1 className='name'> SaladBar </h1>
       </header>
     </div>
+    <form className='form'>
+        <h3>Order ID:434.95451332890076</h3>
+        <label> Username </label> <br/>
+        <input type='text' placeholder=' ' /> 
+        <button>Start my order</button>
+        
+    </form>
+
+    </>
+
   );
+}
+function order(){
+
 }
 
 export default App;
