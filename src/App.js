e738b9cdbf1cfa63bec8c828442f11382e594622
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2022-12-19 17:20:35 +0100

    Menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home  from './components/home';
+import Order from './components/services/services/order';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function App() {
+  const [username, setusername] = useState("")
+
   return (
     <>
+
           <div className="App">
       <header>
         <h1 className='name'> SaladBar </h1>
       </header>
     </div>
-    <form className='form'>
-        <h3>Order ID:434.95451332890076</h3>
-        <label> Username </label> <br/>
-        <input type='text' placeholder=' ' /> 
-        <button>Start my order</button>
-        
-    </form>
+    {username ? username > (<Username username = {Username} />): null }
+    <Routes>
+    <Route path="/" element={<Form/>}/>
+    <Route path="/Order" element={<Order/>} />
+  </Routes>
+   
 
     </>
 
   );
 }
-function order(){
+function Username(props){
+  const {text} = props.username
+  return(
+    <>
+      <p>{text}</p>
+    </>
+  );
+  
 
 }
+function Form(){
+
+  const [username, setusername] = useState("")
+  let navigate = useNavigate();
+
+  return(
+    <>
+      
+        <div id='User'>
+          <h3>Order ID:434.95451332890076</h3>
+          <h3>username:${username}</h3>
+        </div>
+        
+        <form className='form'>
+        <input type='text' placeholder='Username ' value={username} onChange = {e => setusername(e.target.value)} /> 
+        <button  onClick={()=> navigate('/Order')}>Start my order</button>
+
+      </form>
+    </>
+  )
+
+}
+
 
 export default App;
---
+++ src/components/services/services/order.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Order(){
+    return(
+        <>
+            <div>
+            <h3>Order ID:434.95451332890076</h3>
+            <h3>username:</h3>
+            </div>
+            <div id="Order">
+                <form>
+                    <button>Assiettes de crudité</button><br/>
+                    <button>Méli-mélo de crudités</button><br/>
+                    <button>Panaché de crudités</button><br/>
+                    <button>Crudités Bulgare</button><br/>
+                    <button>Salade Arc-en-ciel</button>
+                </form>
+            </div>
+        </>
+
+    )
+}
