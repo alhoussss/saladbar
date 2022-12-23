d94616117d85c37d1ed3f46d4f82e47a486204a2
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2022-12-23 21:18:35 +0100

    Menu terminer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Order from './order';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -63,10 +62,41 @@ function Form(){
   )
 
 }
-function displayPrice(price) {
-  return price;
-}
-var totalPrice = displayPrice(0.99);
-console.log(totalPrice)
 
+function Order(){
+  const [count, setCount] = useState(0);
+  return(
+      <>
+          <div id="User">
+          <h3>Order ID:434.95451332890076</h3>
+          <h3>username:</h3>
+          </div>
+          <div id="Order">
+              <form >
+                <button className="button" onClick={() => setCount(count + 5)}>Assiettes de crudité</button>
+                <button className="boisson" onClick={() => setCount(count + 3)}>Expresso</button>
+                <button className="dessert" onClick={() => setCount(count + 2)}> Macaron</button><br/>
+                <button className="button" onClick={() => setCount(count + 22)}>Méli-mélo de crudités</button>
+                <button className="boisson" onClick={() => setCount(count + 45)}>Chocolat</button>
+                <button className="dessert" onClick={() => setCount(count + 55)}>Île flottante</button><br/>
+                <button className="button" onClick={() => setCount(count + 54)}>Panaché de crudités</button>
+                <button className="boisson" onClick={() => setCount(count + 15)}>Capuccino</button>
+                <button className="dessert" onClick={() => setCount(count + 5)}>mille-feuille</button><br/>
+                <button className="button" onClick={() => setCount(count + 985)}>Crudités Bulgare</button>
+                <button className="boisson" onClick={() => setCount(count + 445)}>Ice-Thea</button>
+                <button className="dessert" onClick={() => setCount(count + 445)}>Tarte au citron</button><br/>
+                <button className="button" onClick={() => setCount(count + 5342)}>Salade Arc-en-ciel</button>
+                <button className="boisson" onClick={() => setCount(count + 4555)}>Jus d'orange</button>
+                <button className="dessert" onClick={() => setCount(count + 45565)}>crème brûlée</button>
+              </form>
+          </div>
+          <div id="invoice">
+
+              <h3>Montant:{count}</h3>
+
+          </div>
+      </>
+
+  );
+}
 export default App;
