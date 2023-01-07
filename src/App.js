34fa2c09f4581c08ee29ffb42adb5e44da59710f
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2023-01-07 22:52:43 +0100

    ON Y AIT PRESQUEX

--- src/App.js
+++ src/App.js
@@ -21,6 +21,11 @@ import Order_six from './Order/Order_six';
 import Order_sept from './Order/Order_sept';
 import Order_huit from './Order/Order_huit';
 import Order_neuf from './Order/Order_neuf';
+import Order_dix from './Order/Order_dix';
+import Order_onze from './Order/Order_onze';
+import Order_douze from './Order/Order_douze';
+import Order_treize from './Order/Order_treize';
+import Order_quatorze from './Order/Order_quatorze';
 
 
 
@@ -47,6 +52,11 @@ function App() {
       <Route path='/CreateUser/MenuCard/Order_sept' element={<Order_sept/>}/>
       <Route path='/CreateUser/MenuCard/Order_huit' element={<Order_huit/>}/>
       <Route path='/CreateUser/MenuCard/Order_neuf' element={<Order_neuf/>}/>
+      <Route path='/CreateUser/MenuCard/Order_dix' element={<Order_dix/>}/>
+      <Route path='/CreateUser/MenuCard/Order_onze' element={<Order_onze/>}/>
+      <Route path='/CreateUser/MenuCard/Order_douze' element={<Order_douze/>}/>
+      <Route path='/CreateUser/MenuCard/Order_treize' element={<Order_treize/>}/>
+      <Route path='/CreateUser/MenuCard/Order_quatorze' element={<Order_quatorze/>}/>
       <Route path='/CreateUser/MenuCard/Order/Recap' element={<Recap/>}/>
       
     </Routes>
@@ -90,26 +100,26 @@ function MenuCard(handlechange){
   return(
       <>
       <div id='nav'>
-          <NavScroll/>
+        <NavScrollExample/>
       </div>
       
           <div id="Card">
               <form >
                 <Button variant="outline-success" className="button" onClick={() => navigate('/CreateUser/MenuCard/Order')}>Assiettes de crudité</Button>
                 <Button variant="outline-success" className="boisson" onClick={() => navigate('/CreateUser/MenuCard/Order_cinq')}>Expresso</Button>
-                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order')}> Macaron</Button><br/>
+                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order_dix')}> Macaron</Button><br/>
                 <Button variant="outline-success" className="button" onClick={() => navigate('/CreateUser/MenuCard/Order_un')}>Méli-mélo de crudités</Button>
                 <Button variant="outline-success" className="boisson" onClick={() => navigate('/CreateUser/MenuCard/Order_six')}>Chocolat</Button>
-                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order')}>Île flottante</Button><br/>
+                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order_onze')}>Île flottante</Button><br/>
                 <Button variant="outline-success" className="button" onClick={() => navigate('/CreateUser/MenuCard/Order_deux')}>Panaché de crudités</Button>
                 <Button variant="outline-success" className="boisson" onClick={() => navigate('/CreateUser/MenuCard/Order_sept')}>Capuccino</Button>
-                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order')}>mille-feuille</Button><br/>
+                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order_douze')}>mille-feuille</Button><br/>
                 <Button variant="outline-success" className="button" onClick={() => navigate('/CreateUser/MenuCard/Order_trois')}>Crudités Bulgare</Button>
                 <Button variant="outline-success" className="boisson" onClick={() => navigate('/CreateUser/MenuCard/Order_huit')}>Ice-Thea</Button>
-                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order')}>Tarte au citron</Button><br/>
+                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order_treize')}>Tarte au citron</Button><br/>
                 <Button variant="outline-success" className="button" onClick={() => navigate('/CreateUser/MenuCard/Order_quatre')}>Salade Arc-en-ciel</Button>
                 <Button variant="outline-success" className="boisson" onClick={() => navigate('/CreateUser/MenuCard/Order_neuf')}>Jus d'orange</Button>
-                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order')}>crème brûlée</Button>
+                <Button variant="outline-success" className="dessert" onClick={() => navigate('/CreateUser/MenuCard/Order_quatorze')}>crème brûlée</Button>
               </form>
           </div>
           <div>
---
+++ src/Order/Order_onze.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "../App.css";
+import { useState } from "react";
+import ingredients from "../ingredients.json";
+import {useNavigate } from "react-router-dom";
+import NavScrollExample from "../Reactbootstrap/navbar";
+import Footer from "../Reactbootstrap/footer";
+import { Button } from "react-bootstrap";
+
+
+function Order_onze() {
+  const [count, setCount] = useState(ingredients[11].Prix_salade)
+  const [cart, setCart] = useState([]);
+  //const [result, setResult] = useState('');
+
+  // fonction pour naviguer vers une autre route
+  let navigate = useNavigate()
+  // fonction pour retirer un ingrédient du panier
+  const removeFromCart = index => {
+    setCart(cart.filter((_, i) => i !== index));
+  }
+  // fonction pour gérer le clic sur un bouton
+  /*const handleClick = () => {
+    setResult(`${ingredients[0]["ingredient"][0]}; ${ingredients[0]["ingredient"][1]}; ${ingredients[0]["ingredient"][2]}; ${ingredients[0]["ingredient"][3]}`);
+  };*/
+  
+  // Reset ma liste
+function resetTodoList() {
+  setCart([]);
+}
+
+  return (
+    <>
+    <div id="nav">
+      <NavScrollExample/>
+    </div>
+    <div id="nav">
+      <div id="ingredients">    
+        <div >
+          <h2>{ingredients && ingredients[11].title}</h2>
+          <img src={ingredients && ingredients[11].photo} alt="Salade" />
+        </div>
+      </div>
+      <div id="Order">
+      <Button variant="outline-success" className="ingre" name="comcombre" onClick={() => setCount(count + ingredients[11].Prix_salade)}> + 1</Button>
+      <Button variant="outline-success" className="ingre" name="comcombre" onClick={() => setCount(count - ingredients[11].Prix_salade)}> - 1</Button>
+        {cart.map((ingredient, index) => (
+          <div key={ingredient.id}>
+            <p>{ ingredient + "  "}{ ingredients[0]["Prix_ingre"]["tomates"]}€ <Button variant="outline-success" className="delete" onClick={async () => { await removeFromCart(index);await setCount(count - ingredients[0]["Prix_ingre"]["tomates"]);}}>Supprimer</Button></p>
+      </div>
+        ))}
+      </div>
+
+      <div id="invoice">
+        <h3>Montant: {count}€</h3>
+      </div>
+      <div id="invoice">
+        <Button variant="outline-success" className="order" onClick={() => navigate('/CreateUser/MenuCard')}> Poursuivre votre commande</Button>
+        <Button variant="outline-success" className="order" onClick={resetTodoList} > Reset</Button>
+        <Button variant="outline-success" className="order" onClick={() => navigate('/CreateUser/MenuCard/Order/Recap')}> Finaliser votre commande >></Button>
+      </div>
+      </div>
+        <Footer/>
+    </>
+  );
+}
+
+export default Order_onze;
